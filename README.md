7be86305c38412274acf9c667f3a27c790619add
Peter Strapp <peter@strapp.co.uk>
2015-11-30 20:34:52 +0000

    Removed global cache and tidied up.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ node-red-contrib-deduplicate
 ============================
 
 Node-RED node that removes duplicate messages.
+
+![Example](example.png)
--- deduplicate.js
+++ deduplicate.js
@@ -1,43 +1,18 @@
 module.exports = function(RED) {
     function DeDuplicate(config) {
         RED.nodes.createNode(this, config);
-        this.name = config.name;
         this.expiry = config.expiry;
         var node = this;
 
-				if (!RED.settings.functionGlobalContext.hasOwnProperty('cache')) {
-					RED.settings.functionGlobalContext.cache = [];
-				}
-
-				function createCacheEntry(key, expiry) {
-					var cacheEntry = {};
-					cacheEntry.expiry = expiry;
-					cacheEntry.key = key;
-					return cacheEntry;
-				}
-
-				function addToCache(cacheName, cacheEntry) {
-					RED.settings.functionGlobalContext.cache[cacheName].push(cacheEntry);
-				}
-
-				function removeFromCache(cacheName, index) {
-					RED.settings.functionGlobalContext.cache[cacheName].splice(index, 1);
-				}
-
-				function initializeCache(cacheName) {
-					RED.settings.functionGlobalContext.cache[cacheName] = [];
-				}
-
-				function cacheContains(cacheName, key, cache)
+				function cacheContains(key)
 				{
-					for(i=0;i<cache.length;i++) {
-						if(cache[i].key == key) {
-							if(expired(cache[i])) {
-								console.log('Entry expired, removing from cache.');
-								removeFromCache(cacheName, i);
-								return false;
+					for(i = 0; i < node.cache.length; i++) {
+						if(node.cache[i].key === key) {
+							if(!expired(node.cache[i])) {
+								return true;
+							} else {
+								node.cache.splice(i, 1);
 							}
-							return true;
 						}
 					}
 					return false;
@@ -48,24 +23,17 @@ module.exports = function(RED) {
 				}
 
         this.on('input', function(msg) {
-          var cacheName = node.name;
-					var cacheExpiry = node.expiry;
-
-					if(typeof(RED.settings.functionGlobalContext.cache[cacheName]) !== 'undefined') {
-						console.log(RED.settings.functionGlobalContext.cache);
-						if(cacheContains(cacheName, JSON.stringify(msg.payload), RED.settings.functionGlobalContext.cache[cacheName])) {
-							console.log('Duplicate message (ignored): ' + RED.settings.functionGlobalContext.cache[cacheName]);
-							return null;
-						} else {
-							console.log('Adding to cache');
-							addToCache(cacheName, createCacheEntry(JSON.stringify(msg.payload), new Date().getTime() + cacheExpiry*1000));
-						}
-					} else {
-						initializeCache(cacheName);
-						addToCache(cacheName, createCacheEntry(JSON.stringify(msg.payload), new Date().getTime() + cacheExpiry*1000));
+					if(typeof(node.cache) === 'undefined') {
+						node.cache = [];
 					}
 
-					node.send(msg);
+					if(cacheContains(JSON.stringify(msg.payload))) {
+						node.send([null, msg]);
+						return;
+					}
+
+					node.cache.push({ expiry: new Date().getTime() + node.expiry * 1000, key: JSON.stringify(msg.payload) });
+					node.send([msg, null]);
         });
     }
     RED.nodes.registerType("deduplicate", DeDuplicate);
