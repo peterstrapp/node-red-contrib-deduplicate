a98758358d510e29be6c91be78a07aa1ab7ea3e9
emms007 <emms007@gmail.com>
2018-07-25 13:35:27 +0200

    Update deduplicate.js
    Extended to allow central list of values management as an option

--- deduplicate.js
+++ deduplicate.js
@@ -4,37 +4,61 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         this.expiry = config.expiry;
         this.keyproperty = config.keyproperty;
+        this.storage = (config.noderole != 'standalone' ? this.context().global : this.context())
+        this.registry = "DeDuplicate_" + (config.registryclass||'')
+
         var node = this;
 
         function expired(entry) {
             return new Date().getTime() > entry.expiry;
         }
 
-        function cacheContains(key) {
+        function cacheContains(topic,key) {
             var i;
-            for (i = 0; i < node.cache.length; i += 1) {
-                if (node.cache[i].key === key) {
-                    if (!expired(node.cache[i])) {
+            var known_entries;
+
+            known_entries = node.storage.get(node.registry+'["'+topic+'"]')
+
+            for (i = 0; i < known_entries.length; i += 1) {
+                if (known_entries[i].key === key) {
+                    if (!expired(known_entries[i])) {
                         return true;
                     }
-                    node.cache.splice(i, 1);
+                    known_entries.splice(i, 1);
+                    node.storage.set(node.registry+'["'+topic+'"]',known_entries)
                 }
             }
             return false;
         }
 
         this.on('input', function (msg) {
-            if (node.cache === undefined) {
-                node.cache = [];
-            }
+
 
             var key = node.keyproperty ? msg.payload[node.keyproperty] : msg.payload;
-            if (cacheContains(JSON.stringify(key))) {
+            var topic = (msg.topic || "")
+
+            if (node.storage.get(node.registry) === undefined) {
+                node.storage.set(node.registry,{})
+            }
+
+            if (node.storage.get(node.registry+'["'+topic+'"]') === undefined) {
+                node.storage.set(node.registry+'["'+topic+'"]',[])
+            }
+
+
+            if (cacheContains(topic, JSON.stringify(key))) {
                 node.send([null, msg]);
                 return;
             }
 
-            node.cache.push({expiry: new Date().getTime() + node.expiry * 1000, key: JSON.stringify(key)});
+
+            var known_values  = node.storage.get(node.registry+'["'+topic+'"]')
+
+            if (config.noderole != "deduplicate") {
+                known_values.push({expiry: new Date().getTime() + node.expiry * 1000, key: JSON.stringify(key)});
+                node.storage.set(node.registry+'["'+topic+'"]',known_values)
+            }
+
             node.send([msg, null]);
         });
     }
